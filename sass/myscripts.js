03693fb1879d172fb7ac0b196fcaa862bf930a66
Veer Jadimath <veerujadimath@gmail.com>
2022-09-23 17:06:18 +0530

    Add files via upload

---
+++ sass/myscripts.js
@@ -0,0 +1,9 @@
+document.getElementById("one").addEventListener("click", myFunction());
+
+function myFunction() {
+    alert("hello world");
+}
+
+// function removeHandler() {
+// document.getElementById("unoo").removeEventListener("click", myfunction());
+// }   
